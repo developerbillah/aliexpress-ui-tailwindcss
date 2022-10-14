43053dfd03e4978da74024bea7b9bd64d125dbe7
Md Masum Billah <masum@deligram.com>
2022-10-14 18:53:10 +0600

    Improve Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       openSans : ['Open Sans', 'sans-serif'],
     },
     extend: {
+      backgroundImage: {
+        'discount': "url('./dist/assets/img/13.jpeg')",
+        'memberOffer': "url('https://ae01.alicdn.com/kf/H863bb19ae5de49a8bd6074a3d81599d9z.jpg_Q90.jpg_.webp')",
+       },
       colors: {
         redPraimay : "#FA5156",
         redSecondary: "#FB6A3A"
